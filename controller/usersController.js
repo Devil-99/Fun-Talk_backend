ba77ac987fd1df7113a3615b1c4d35fd4b0bc328
Maji <souvmaji@deloitte.com>
2024-11-29 17:33:16 +0530

    getUsers api modification

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,12 +1,18 @@
 const pool = require('../db');
 
-const getUsers = async ({user_id}) => {
+const getUsers = async ({ user_id }) => {
     try {
-        const result = await pool.query('SELECT user_id, username FROM users WHERE user_id != $1',[user_id]);
+        const result = await pool.query('SELECT user_id, username, dp FROM users WHERE user_id != $1',[user_id]);
         if (result.rows.length > 0) {
+            // Transforming the result to include dp in base64 format
+            const transformedData = result.rows.map(user => ({
+                user_id: user.user_id,
+                username: user.username,
+                dp: user.dp ? `data:image/png;base64,${user.dp.toString('base64')}` : null,
+            }));
             return {
                 status: 200,
-                data: result.rows
+                data: transformedData
             }
         } else {
             return {
